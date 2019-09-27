4e3c900442a0888953e3dece0efc1db89f46c468
mattShaverWebCreator <matt@boostthegram.com>
2019-09-27 00:25:13 -0400

    Updated form.

--- src/components/Main.js
+++ src/components/Main.js
@@ -187,7 +187,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" netlify>
+          <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
