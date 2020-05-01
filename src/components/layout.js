6ff4d2ba85581dc0dc2fcdbff449727a6bec7c7b
Matthew Shaver <matthewshaver@Matthews-Air.mshome.net>
2020-05-01 20:57:41 +0200

    Updated meta information in layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,11 +43,11 @@ const Layout = ({ children, location }) => {
             title={data.site.siteMetadata.title}
             meta={[
               { name: 'description', content: data.site.siteMetadata.description },
-              { property: 'og:url', content: 'https://websitedesignnorthcarolina.com'},
+              { property: 'og:url', content: 'https://mattshaverwebcreator.com'},
               { property: 'og:type', content: 'website'},
               { property: 'og:title', content: "Matt's Portfolio"},
               { property: 'og:description', content: data.site.siteMetadata.description },
-              { property: 'og:image', content: data.site.siteMetadata.image}
+              { property: 'og:image', content: 'https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/s960x960/71803732_952924578395500_104562138462289920_o.jpg?_nc_cat=105&_nc_sid=dd9801&_nc_ohc=5s-PsDsWpmMAX94z7dH&_nc_ht=scontent.fbeg4-1.fna&_nc_tp=7&oh=3d2f74ed3190afe6d4da390c2d15289f&oe=5ED01DE3'}
             ]}
           >
           </Helmet>
