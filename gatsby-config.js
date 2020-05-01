89dd04bdea13e8996a2deb9ac201cd2bec4a2b46
Matthew Shaver <matthewshaver@Matthews-Air.mshome.net>
2020-05-01 20:50:56 +0200

    Updated meta information.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ module.exports = {
     title: "Matt's Portfolio",
     author: "Matthew Shaver",
     description: "Matt Shaver is a Front End Developer with a passion for creating amazing UI/UX experiences.",
-    image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/71803732_952924578395500_104562138462289920_o.jpg?_nc_cat=105&_nc_oc=AQkBMEZjLfn3YEb_qXXh3Sf8Asw1Uuw8kstQyJdH8RfdkEEMyQ6evAkFp71MAlH7vDY&_nc_ht=scontent-lga3-1.xx&oh=8f68b69b21f9d329f9e876b5ed365d8f&oe=5E36C8C5',
-    url: 'https://websitedesignnorthcarolina.com'
+    image: 'https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/s960x960/71803732_952924578395500_104562138462289920_o.jpg?_nc_cat=105&_nc_sid=dd9801&_nc_ohc=5s-PsDsWpmMAX94z7dH&_nc_ht=scontent.fbeg4-1.fna&_nc_tp=7&oh=3d2f74ed3190afe6d4da390c2d15289f&oe=5ED01DE3',
+    url: 'https://mattshaverwebcreator.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
